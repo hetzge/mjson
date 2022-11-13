28d9b743a4a934b92bba2fc0d0f067877bb6565d
hetzge <hetzge@gmx.de>
2022-11-13 11:56:39 +0100

    Format

--- src/test/java/testmjson/TestJsonSchemaSuite.java
+++ src/test/java/testmjson/TestJsonSchemaSuite.java
@@ -17,78 +17,59 @@ import mjson.Json;
 /**
  * Run a test from the https://github.com/json-schema/JSON-Schema-Test-Suite
  * test spec.
- * 
+ *
  * @author Borislav Iordanov
  *
  */
 @RunWith(Parameterized.class)
-public class TestJsonSchemaSuite
-{
-	private String group;
-	private String description;
-	private Json.Schema schema;
-	private Json data;
-	private boolean valid;
-	
-	 @Parameters
-     public static Collection<Object[]> data() {    	 
- 		List<Object[]> tests = new ArrayList<Object[]>(); 
- 		for (Map.Entry<String, String> test : TestSchemas.testResources("suite").entrySet())
- 		{
- 			Json set = Json.read(test.getValue());
- 			if (!set.isArray()) set = Json.array().add(set);
- 			for (Json one : set.asJsonList())
- 			{
- 				try
- 				{
- 					Json.Schema schema = Json.schema(one.at("schema"));
- 					for (Json t : one.at("tests").asJsonList())
- 						tests.add(new Object[]{test.getKey(),
- 													t.at("description","***").asString() + "/" +
- 								  					      one.at("description", "---").asString(),
- 								  					schema,
- 								  					t.at("data"),
- 								  					t.at("valid", true).asBoolean()});
- 				}
- 				catch (Throwable t)
- 				{
- 					throw new RuntimeException("While adding tests from file " + test.getKey() + " - " + one, t);
- 				}
- 			}
- 		}
- 		return tests;
+public class TestJsonSchemaSuite {
+  private final String group;
+  private final String description;
+  private final Json.Schema schema;
+  private final Json data;
+  private final boolean valid;
+
+  @Parameters
+  public static Collection<Object[]> data() {
+    final List<Object[]> tests = new ArrayList<Object[]>();
+    for (final Map.Entry<String, String> test : TestSchemas.testResources("suite").entrySet()) {
+      Json set = Json.read(test.getValue());
+      if (!set.isArray()) {
+        set = Json.array().add(set);
+      }
+      for (final Json one : set.asJsonList()) {
+        try {
+          final Json.Schema schema = Json.schema(one.at("schema"));
+          for (final Json t : one.at("tests").asJsonList()) {
+            tests.add(new Object[] { test.getKey(), t.at("description", "***").asString() + "/" + one.at("description", "---").asString(), schema, t.at("data"), t.at("valid", true).asBoolean() });
+          }
+        } catch (final Throwable t) {
+          throw new RuntimeException("While adding tests from file " + test.getKey() + " - " + one, t);
+        }
+      }
     }
-	    
-	public TestJsonSchemaSuite(String group, 
-						 String description, 
-						 Json.Schema schema, 
-						 Json data, 
-						 boolean valid)
-	{
-		this.group = group;
-		this.description = description;
-		this.schema = schema;
-		this.data = data;
-		this.valid = valid;
-	}
-	
-	@Test
-	public void doTest()
-	{
-		try
-		{
-			System.out.println("Running test " + description + " from " + group);
-			Assert.assertEquals("Running test " + description + " from " + group,
-							valid, schema.validate(data).is("ok", true));
-		}
-		catch (AssertionFailedError err) // caught so we can break in with debugger here...
-		{
-			schema.validate(data);
-			throw err;
-		}
-		catch (Throwable t)
-		{
-			System.err.println("Exception while running test " + description + " from " + group);			
-		}
-	}	
+    return tests;
+  }
+
+  public TestJsonSchemaSuite(String group, String description, Json.Schema schema, Json data, boolean valid) {
+    this.group = group;
+    this.description = description;
+    this.schema = schema;
+    this.data = data;
+    this.valid = valid;
+  }
+
+  @Test
+  public void doTest() {
+    try {
+      System.out.println("Running test " + this.description + " from " + this.group);
+      Assert.assertEquals("Running test " + this.description + " from " + this.group, this.valid, this.schema.validate(this.data).is("ok", true));
+    } catch (final AssertionFailedError err) // caught so we can break in with debugger here...
+    {
+      this.schema.validate(this.data);
+      throw err;
+    } catch (final Throwable t) {
+      System.err.println("Exception while running test " + this.description + " from " + this.group);
+    }
+  }
 }
